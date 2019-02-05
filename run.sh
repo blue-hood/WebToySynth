5202076700eafda3307e7c96fccdaa45d520a7fc
hato6502 <erisuke12345@yahoo.co.jp>
2019-02-05 23:18:07 +0900

    Add docker config file.

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+
+. ./config
+
 docker build ./ -t troopa
-docker run -d --name troopa -v `pwd`/html:/var/www/html troopa
+docker run -d --name troopa -v `pwd`/html:/var/www/html -p ${PORT}:80 troopa
