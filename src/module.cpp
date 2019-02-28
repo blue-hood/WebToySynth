38b6559162739b066221323c57e05c263b739716
hato6502 <erisuke12345@yahoo.co.jp>
2019-02-28 21:44:29 +0900

    C++ の高速化をする前にバックアップコミットします。

--- src/module.cpp
+++ src/module.cpp
@@ -30,8 +30,17 @@ int main(int argc, char **argv)
     Component_p sine1 = Component_p(newCom("Sine"));
     g_sketch.appendCom(sine1);
 
-    speaker1->ins[speaker1->getIn()["sound"]]->connect(sine1->outs[sine1->getOut()["sine"]]);
     sine1->ins[sine1->getIn()["freq"]]->connect(input1->outs[input1->getOut()["value"]]);
+    speaker1->ins[speaker1->getIn()["sound"]]->connect(sine1->outs[sine1->getOut()["sine"]]);
+    for (int i = 1; i < 50 /*speakerの数*/; i++)
+    {
+        Component_p speaker2 = Component_p(newCom("Speaker"));
+        g_sketch.appendCom(speaker2);
+
+        speaker2->ins[speaker2->getIn()["sound"]]->connect(speaker1->outs[speaker1->getOut()["thru"]]);
+
+        speaker1 = speaker2;
+    }
 
     static_cast<Input *>(input1.get())->setValue(440.0);
 
@@ -53,7 +62,7 @@ EMSCRIPTEN_KEEPALIVE float *onAudioProcess(double dt)
             g_spout = 0.0;
             g_spcount = 0;
             g_sketch.onChangeTime(dt);
-            g_buffer[i] = g_spcount != 0 ? g_spout / g_spcount : 0.0;
+            g_buffer[i] = g_spcount ? g_spout / g_spcount : 0.0f;
         }
     }
     catch (exception &e)
